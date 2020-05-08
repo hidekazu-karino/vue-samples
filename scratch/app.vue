6c98f97d2936a81a85c5797ed546eca0f3879ba3
EC2 Default User <ec2-user@ip-10-0-0-232.ap-northeast-1.compute.internal>
2020-05-08 13:53:03 +0000

    単一ファイルでボタンをクリックするとインクリメントするように

---
+++ scratch/app.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1>Hello!</h1>
+    <p>
+      {{ number }}
+    </p>
+    <hr>
+    <!-- 単一ファイルコンポーネントだと@でイベントを検知してメソッドを呼ぶ-->
+    <button @click="increment">
+        click
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  el: "#app",
+  // dataは関数にしてリターンでオブジェクトを返すようにする．
+  data() {
+    return {
+        number: 0
+    }
+  },
+  methods: {
+      increment() {
+          this.number += 1
+      }
+  }
+};
+</script>
