09fb4efb1b49bd55ffb526c84b79758966959485
hidekazu-karino <uzakedih.4869@gmail.com>
2020-05-09 06:21:06 +0000

    コンポーネントの簡単な例

---
+++ components_study/button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button v-on:click="count++">You clicked me {{ count }} times.</button>
+</template>
+
+<script>
+// export defaultのなかにtemplateを仕込んでも反映されなかった．
+export default {
+  name: "button",
+  data: function() {
+    return {
+      count: 0
+    };
+  },
+  template: "<p>Hello</p>"
+};
+</script>
---
+++ components_study/hello.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <buttoncounter />
+  </div>
+</template>
+
+<script>
+//importの相対パスは以下のような感じ
+import buttoncounter from "./button.vue";
+export default {
+  name: "#app",
+  components: {
+    buttoncounter
+  }
+};
+</script>
