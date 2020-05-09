c70e31654b4178ab22fb15716c6474bae67faada
hidekazu-karino <uzakedih.4869@gmail.com>
2020-05-09 06:44:53 +0000

    propsでデータを親から子へ渡して表示する

--- components_study/hello.vue
+++ components_study/hello.vue
@@ -1,16 +1,36 @@
 <template>
   <div id="app">
-    <buttoncounter />
+    <div>
+      <h3>ボタンを読み込む</h3>
+      <buttoncounter />
+    </div>
+    <div>
+      <h3>propsでデータを渡してアイテムを表示する．</h3>
+      <p>List</p>
+      <ul v-for="item in items" :key="item">
+        <showItem :name="item.name" :price="item.price"></showItem>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 //importの相対パスは以下のような感じ
 import buttoncounter from "./button.vue";
+import showItem from "./item.vue";
 export default {
   name: "#app",
   components: {
-    buttoncounter
+    buttoncounter,
+    showItem
+  },
+  data() {
+    return {
+      items: [
+        { name: "ぶどう", price: 500 },
+        { name: "apple", price: 300 }
+      ]
+    };
   }
 };
 </script>
---
+++ components_study/item.vue
@@ -0,0 +1,11 @@
+<template>
+  <li>{{ name }}: {{ price }}円</li>
+</template>
+ 
+<script>
+export default {
+  name: "Item",
+  // 親コンポーネントから受け取るpropsを定義.
+  props: ["name", "price"]
+};
+</script>
