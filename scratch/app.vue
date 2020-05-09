d2b17461a14ff634d1e169bb4014f6db832beddc
hidekazu-karino <uzakedih.4869@gmail.com>
2020-05-09 04:04:59 +0000

    renderを用いた描画を追加

--- scratch/app.vue
+++ scratch/app.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="app">
     <h1>Hello!</h1>
-    <p>
-      {{ number }}
-    </p>
-    <br>
+    <p>{{ number }}</p>
+    <br />
     <!-- 単一ファイルコンポーネントだと@でイベントを検知してメソッドを呼ぶ-->
-    <button @click="increment">
-        click
-    </button>
-    <hr>
+    <button @click="increment">click</button>
+    <hr />
     <!-- formのinputなど双方向バインディングにはv-modelを使う．-->
     <!-- 入力されたイベントを検知してメソッドを呼ぶみたいなことをしなくていい．-->
-    <input v-model="message" placeholder="edit me">
+    <input v-model="message" placeholder="edit me" />
     <p>{{ message }}</p>
+    <hr />
+    {{ message_render }}
   </div>
 </template>
 
@@ -23,14 +21,18 @@ export default {
   // dataは関数にしてリターンでオブジェクトを返すようにする．
   data() {
     return {
-        number: 0,
-        message: "please type!"
-    }
+      number: 0,
+      message: "please type!",
+      message_render: "Hello Vue!"
+    };
+  },
+  render: element => {
+    return element("p", this.message_render);
   },
   methods: {
-      increment() {
-          this.number += 1
-      }
+    increment() {
+      this.number += 1;
+    }
   }
 };
 </script>
