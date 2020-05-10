9b8bdc8f020309eebc3a9d77e62def4c39e0e9c5
hidekazu-karino <uzakedih.4869@gmail.com>
2020-05-10 06:18:51 +0000

    Nuxt.js initial commit. パラメータの取得サンプル

---
+++ nuxt_sample/pages/p/_id/_pass.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="container">
+    <h2>{{ title }}</h2>
+    <p v-html="message">no message.</p>
+    <hr />
+    <div class="right">
+      <router-link to="/">Go to Top</router-link>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      title: "Login"
+    };
+  },
+  computed: {
+    message: function() {
+      let id =
+        this.$route.params.id != undefined
+          ? this.$route.params.id
+          : "*** no id ***";
+      let pass =
+        this.$route.params.id != undefined
+          ? this.$route.params.pass
+          : "*** no password ***";
+      return "ID: " + id + "<br>PASS: " + pass;
+    }
+  }
+};
+</script>
