356653a157ad179f92758183ca29d0feaf499156
hidekazu-karino <uzakedih.4869@gmail.com>
2020-05-10 04:22:27 +0000

    イベント処理を別途用意する

--- components_study/button.vue
+++ components_study/button.vue
@@ -1,5 +1,10 @@
 <template>
-  <button v-on:click="count++">You clicked me {{ count }} times.</button>
+  <div>
+    <h3>クリックした回数を表示する．</h3>
+    <button v-on:click="count++">You clicked me {{ count }} times.</button>
+    <h3>クリックした数に応じて色を変える．</h3>
+    <button v-bind:class="{red:isRed}" v-on:click="doAction">clicked: {{ counter }}</button>
+  </div>
 </template>
 
 <script>
@@ -8,9 +13,30 @@ export default {
   name: "button",
   data: function() {
     return {
-      count: 0
+      count: 0,
+      counter: 0,
+      isRed: false
     };
   },
-  template: "<p>Hello</p>"
+  methods: {
+    doAction: function() {
+      this.counter++;
+      if (this.counter > 10) {
+        this.counter = 0;
+      }
+      if (this.counter % 2 == 0) {
+        this.isRed = false;
+      } else {
+        this.isRed = true;
+      }
+    }
+  }
 };
 </script>
+
+<style>
+.red {
+  color: white;
+  background-color: red;
+}
+</style>
