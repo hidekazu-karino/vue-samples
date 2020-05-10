f851c654cb817f0a190890c9a88c30eb4f9f3656
hidekazu-karino <uzakedih.4869@gmail.com>
2020-05-10 04:52:27 +0000

    v-bindで親のデータとこのデータをバインディングすることで親から子へデータを渡す

---
+++ components_study/child.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h3>{{ title }}</h3>
+    <p>{{ message }}</p>
+    <hr />
+    <div>
+      <input type="text" v-model="input" />
+      <button v-on:click="doAction">Click</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "child",
+  props: {
+    title: String
+  },
+  data: function() {
+    return {
+      message: "What's your name?",
+      input: "no name"
+    };
+  },
+  methods: {
+    doAction: function() {
+      this.message = "Hello, Mr " + this.input;
+    }
+  }
+};
+</script>
--- components_study/hello.vue
+++ components_study/hello.vue
@@ -11,26 +11,40 @@
         <showItem :name="item.name" :price="item.price"></showItem>
       </ul>
     </div>
+    <div>
+      <!-- 子コンポーネントのプロパティにv-bindで親のプロパティをバインディングすれば親から子へデータを渡せる-->
+      <helloworld v-bind:title="message" />
+      <button v-on:click="sendTitle">change title</button>
+    </div>
   </div>
 </template>
 
 <script>
 //importの相対パスは以下のような感じ
-import buttoncounter from "./button.vue";
+import buttoncounter from "./buttonc.vue";
 import showItem from "./item.vue";
+import helloworld from "./child.vue";
 export default {
   name: "#app",
   components: {
     buttoncounter,
-    showItem
+    showItem,
+    helloworld
   },
   data() {
     return {
       items: [
         { name: "ぶどう", price: 500 },
         { name: "apple", price: 300 }
-      ]
+      ],
+      message: "HELLO"
     };
+  },
+  methods: {
+    sendTitle: function() {
+      let input = prompt("new title:");
+      this.message = input;
+    }
   }
 };
 </script>
